db956f68678cb299d96fb6f7014fab37bab04e50
Mhaks <151679700+Mhaks02@users.noreply.github.com>
2026-02-17 23:20:20 +0100

    Unused import statement removed

--- src/main/java/io/github/apace100/cosmetic_armor/CosmeticArmor.java
+++ src/main/java/io/github/apace100/cosmetic_armor/CosmeticArmor.java
@@ -1,17 +1,19 @@
 package io.github.apace100.cosmetic_armor;
 
-import dev.emi.trinkets.api.*;
+import dev.emi.trinkets.api.SlotReference;
+import dev.emi.trinkets.api.SlotType;
+import dev.emi.trinkets.api.TrinketComponent;
+import dev.emi.trinkets.api.TrinketsApi;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.util.TriState;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.registry.tag.*;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Pair;
-import net.minecraft.registry.RegistryKeys;
 
 import java.util.List;
 import java.util.Optional;
