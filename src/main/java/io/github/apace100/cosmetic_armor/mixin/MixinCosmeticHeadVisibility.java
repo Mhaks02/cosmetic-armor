865b082c13ad6962aa0d920df0790a3e5490ae86
Mhaks <151679700+Mhaks02@users.noreply.github.com>
2026-02-18 01:11:44 +0100

    Changed mixins and fixed errors in cosmetics jsons

--- src/main/java/io/github/apace100/cosmetic_armor/mixin/MixinCosmeticHeadVisibility.java
+++ src/main/java/io/github/apace100/cosmetic_armor/mixin/MixinCosmeticHeadVisibility.java
@@ -3,20 +3,23 @@ package io.github.apace100.cosmetic_armor.mixin;
 import io.github.apace100.cosmetic_armor.CosmeticArmor;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.render.entity.feature.HeadFeatureRenderer;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.client.render.entity.state.BipedEntityRenderState;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Environment(EnvType.CLIENT)
-@Mixin(HeadFeatureRenderer.class)
-public class MixinCosmeticHeadVisibility {
+@Mixin(value = LivingEntityRenderer.class)
+public class MixinCosmeticHeadVisibility<T extends MobEntity, S extends BipedEntityRenderState, M extends BipedEntityModel<S>> {
 
-	@Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getEquippedStack(Lnet/minecraft/entity/EquipmentSlot;)Lnet/minecraft/item/ItemStack;"))
-	private ItemStack modifyVisibleHead(LivingEntity entity, EquipmentSlot slot) {
+	@Redirect(method = "updateRenderState", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getEquippedStack(Lnet/minecraft/entity/EquipmentSlot;)Lnet/minecraft/item/ItemStack;"))
+	private static ItemStack modifyVisible(LivingEntity entity, EquipmentSlot slot) {
 		ItemStack equippedStack = entity.getEquippedStack(slot);
 		ItemStack cosmeticStack = CosmeticArmor.getCosmeticArmor(entity, slot);
 		if(!cosmeticStack.isEmpty() && (equippedStack.isEmpty() || !equippedStack.isIn(CosmeticArmor.ALWAYS_VISIBLE))) {
